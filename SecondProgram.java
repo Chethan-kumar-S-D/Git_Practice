d210affa0eb371e19b154c45885adfe8001903c0
Chethan-kumar-S-D <ck.cece16@gmail.com>
2023-03-22 19:52:43 +0530

    Second commit

---
+++ SecondProgram.java
@@ -0,0 +1,13 @@
+package com.git.app1;
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println( "hi" );
+		}
+
+	}
+
+}
